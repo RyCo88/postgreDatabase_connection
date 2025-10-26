ec1cad0ccd18d90862f2a6c28bcea9c30664f156
Ryan Corson <ryan.corson88@gmail.com>
2025-10-26 17:29:36 -0500

    builds PostgreDatabaseApplication.java file

--- src/main/java/com/ryco/postgreDatabase/PostgreDatabaseApplication.java
+++ src/main/java/com/ryco/postgreDatabase/PostgreDatabaseApplication.java
@@ -1,13 +1,31 @@
 package com.ryco.postgreDatabase;
 
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class PostgreDatabaseApplication {
+@Log
+public class PostgreDatabaseApplication implements CommandLineRunner {
+
+    private final DataSource dataSource;
+
+    public PostgreDatabaseApplication(final DataSource dataSource)  {
+        this.dataSource = dataSource;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(PostgreDatabaseApplication.class, args);
 	}
 
+    @Override
+    public void run(final String... args) {
+        log.info("Datasource: " + dataSource.toString());
+        final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+        restTemplate.execute("select 1");
+    }
 }
