4d63a8c0cbd4d814d404c2ba95198bbe12f63c98
Ryan Corson <ryan.corson88@gmail.com>
2025-10-26 16:28:44 -0500

    files created with Spring Initializer

---
+++ src/main/java/com/ryco/postgreDatabase/PostgreDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.ryco.postgreDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgreDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgreDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryco/postgreDatabase/PostgreDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryco.postgreDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgreDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
